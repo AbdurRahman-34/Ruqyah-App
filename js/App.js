0d0f0f50801b84a7c793774b8e89b9d23416fc4a
AbdurRahman-34 <get.abdurahman@gmail.com>
2022-08-28 21:54:25 +0600

    js update file

---
+++ js/App.js
@@ -0,0 +1,16 @@
+const wrapper = document.querySelector(".wrapper"),
+musicImg = wrapper.document.querySelector(".img-area img"),
+musicName = wrapper.document.querySelector("song-details .name"),
+musicArtist = wrapper.document.querySelector("song-details .artist");
+
+
+let musicIndex = 2;
+window.addEventListener("load", () => {
+    loadMusic(musicIndex);  // calling load Function once windows loaded
+})
+
+
+// Load Music Function :::::
+const loadMusic = () {
+
+}
--- js/music-list.js
+++ js/music-list.js
@@ -34,11 +34,6 @@ let allMusic = [
         artist : "",
         img: "music-1",
         src: "music-1"
-    },
-    {
-        name: 'Abdur Rahman',
-        artist : "",
-        img: "music-1",
-        src: "music-1"
-    },
+    }
 ]
+
