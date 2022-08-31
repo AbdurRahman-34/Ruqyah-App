7c5302a7c1e830a4a0c644b195051b844a3a917d
AbdurRahman-34 <get.abdurahman@gmail.com>
2022-08-31 12:27:17 +0600

    js file update1

--- js/App.js
+++ js/App.js
@@ -1,16 +1,21 @@
 const wrapper = document.querySelector(".wrapper"),
-musicImg = wrapper.document.querySelector(".img-area img"),
-musicName = wrapper.document.querySelector("song-details .name"),
-musicArtist = wrapper.document.querySelector("song-details .artist");
+musicImg = wrapper.querySelector(".img-area img"),
+musicName = wrapper.querySelector(".song-details .name"),
+musicArtist = wrapper.querySelector(".song-details .artist"),
+mainAudio = wrapper.querySelector("#main-audio");
 
 
-let musicIndex = 2;
-window.addEventListener("load", () => {
+let musicIndex = 6;
+window.addEventListener("load", ()=> {
     loadMusic(musicIndex);  // calling load Function once windows loaded
 })
 
 
 // Load Music Function :::::
-const loadMusic = () {
-
+function loadMusic(indexNumb){
+    musicName.innerText = allMusic[indexNumb -1].name;
+    musicArtist.innerText = allMusic[indexNumb -1].artist;
+    musicImg.src = `images/${allMusic[indexNumb -1].img}.jpg`;
+    mainAudio.src = `songs/${allMusic[indexNumb -1].src}.mp3`;
 }
+
--- js/music-list.js
+++ js/music-list.js
@@ -1,39 +1,39 @@
 let allMusic = [
     {
         name: 'Abdur Rahman',
-        artist : "sura",
+        artist : "sura1",
         img: "music-1",
         src: "music-1"
     },
     {
         name: 'Hifzur Rahman',
-        artist : "sura",
-        img: "music-1",
-        src: "music-1"
+        artist : "sura2",
+        img: "music-2",
+        src: "music-2"
     },
     {
-        name: 'Abdur Rahman',
-        artist : "",
-        img: "music-1",
-        src: "music-1"
+        name: 'Abdur Rahman2',
+        artist : "sura3",
+        img: "music-3",
+        src: "music-3"
     },
     {
-        name: 'Abdur Rahman',
-        artist : "",
-        img: "music-1",
-        src: "music-1"
+        name: 'Abdur Rahman3',
+        artist : "sura4",
+        img: "music-4",
+        src: "music-4"
     },
     {
-        name: 'Abdur Rahman',
-        artist : "",
-        img: "music-1",
-        src: "music-1"
+        name: 'Abdur Rahman4',
+        artist : "sura5",
+        img: "music-5",
+        src: "music-5"
     },
     {
-        name: 'Abdur Rahman',
-        artist : "",
-        img: "music-1",
-        src: "music-1"
+        name: 'Abdur Rahman5',
+        artist : "sura6",
+        img: "music-6",
+        src: "music-6"
     }
 ]
 
