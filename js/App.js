2cd35ce82797c9dafc0e7ea5951d14e12da1fe27
AbdurRahman-34 <get.abdurahman@gmail.com>
2022-08-31 13:17:50 +0600

    js play button update

--- js/App.js
+++ js/App.js
@@ -2,7 +2,8 @@ const wrapper = document.querySelector(".wrapper"),
 musicImg = wrapper.querySelector(".img-area img"),
 musicName = wrapper.querySelector(".song-details .name"),
 musicArtist = wrapper.querySelector(".song-details .artist"),
-mainAudio = wrapper.querySelector("#main-audio");
+mainAudio = wrapper.querySelector("#main-audio"),
+playPauseBtn = wrapper.querySelector(".play-pause");
 
 
 let musicIndex = 6;
@@ -19,3 +20,26 @@ function loadMusic(indexNumb){
     mainAudio.src = `songs/${allMusic[indexNumb -1].src}.mp3`;
 }
 
+
+// Play Music Function 
+function playMusic(){
+    wrapper.classList.add("paused");
+    playPauseBtn.querySelector("i").innerText = "pause";
+    mainAudio.play()
+}
+
+
+// Pause Music Function 
+function pauseMusic(){
+    wrapper.classList.remove("paused");
+    playPauseBtn.querySelector("i").innerText = "play_arrow";
+    mainAudio.pause()
+}
+
+
+
+// Play or Tilawat button even
+playPauseBtn.addEventListener("click", () =>{
+    const isMusicPasued = wrapper.classList.contains("paused");
+    isMusicPasued ? pauseMusic() : playMusic()
+})
